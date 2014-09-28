2b2d518012725cb3335178d3246591cecd02981b
Mark Johnston <mjj@mintydog.com>
2014-09-28 21:26:06 +0100

    Added Empty BaseModel constructor

--- opennlp.tools/src/util/model/BaseModel.cs
+++ opennlp.tools/src/util/model/BaseModel.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using opennlp.tools.sentdetect;
 
 namespace opennlp.tools.util.model
 {
     public class BaseModel
     {
+        protected BaseModel(string componentName, string languageCode, IDictionary<string, string> manifestInfoEntries, SentenceDetectorFactory sdFactory)
+        {
+            throw new NotImplementedException();
+        }
+
         protected internal virtual void validateArtifactMap()
         {
             throw new NotImplementedException();
