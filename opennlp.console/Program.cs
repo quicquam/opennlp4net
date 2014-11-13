86d497a2c1acab523ee5a886460aaafde89c7461
Mark Johnston <mark.johnston@euromoney.com>
2014-11-13 16:58:37 +0000

    Command line processing for console

--- opennlp.console/Program.cs
+++ opennlp.console/Program.cs
@@ -10,6 +10,20 @@ namespace opennlp.console
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // consume Options instance properties
+                if (string.IsNullOrEmpty(options.ToolName))
+                {
+                    Console.WriteLine("No toolName specified");
+                }
+                else
+                {
+                    var opennlpCore = new OpenNlpCore(options);
+                    opennlpCore.Process();
+                }
+            }
         }
     }
 }
