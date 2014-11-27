7dab9f82ad9fbadfd32a83cb1d572507d9d6f8cb
Mark Johnston <mark.johnston@euromoney.com>
2014-11-27 13:55:50 +0000

    Updated parserTests

--- opennlp.tools.Tests/src/parserTests.cs
+++ opennlp.tools.Tests/src/parserTests.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using j4n.IO.InputStream;
 using NUnit.Framework;
-using j4n.IO.InputStream;
 using opennlp.tools.parser;
+using System.Linq;
+using System.Text;
 
 namespace opennlp.tools.Tests
 {
@@ -32,8 +33,15 @@ namespace opennlp.tools.Tests
             var model = new ParserModel(modelIn);
             var parser = ParserFactory.create(model);
 
-            var sentence = "The quick brown fox jumps over the lazy dog .";
-            var topParses = StandAloneParserTool.parseLine(sentence, parser, 1);
+            const string sentence = "The quick brown fox jumps over the lazy dog .";
+            const string correctParse = "(TOP (NP (NP (DT The) (JJ quick) (JJ brown) (NN fox) (NNS jumps)) (PP (IN over) (NP (DT the) (JJ lazy) (NN dog))) (. .)))";
+            var sb = new StringBuilder();
+            var topParse = StandAloneParserTool.parseLine(sentence, parser, 1).FirstOrDefault();
+            if (topParse != null)
+            {
+                topParse.show(sb);
+            }
+            Assert.AreEqual(correctParse, sb.ToString());
 
             modelIn.close();
         }
--- opennlp.tools/src/parser/StandAloneParserTool.cs
+++ opennlp.tools/src/parser/StandAloneParserTool.cs
@@ -1,49 +1,36 @@
-﻿using System.Collections.Generic;
-using System.Text;
-using j4n.Lang;
-using j4n.Object;
+﻿using j4n.Object;
 using opennlp.tools.util;
+using System.Collections.Generic;
+using System.Text;
 
 namespace opennlp.tools.parser
 {
     public static class StandAloneParserTool
     {
-        private static Pattern untokenizedParentPattern1 = Pattern.compile("([^ ])([({)}])");
-        private static Pattern untokenizedParentPattern2 = Pattern.compile("([({)}])([^ ])");
 
         public static Parse[] parseLine(string line, Parser parser, int numParses)
         {
-            //line = untokenizedParentPattern1.matcher(line).replaceAll("$1 $2");
-            //line = untokenizedParentPattern2.matcher(line).replaceAll("$1 $2");
-            StringTokenizer str = new StringTokenizer(line);
-            StringBuilder sb = new StringBuilder();
-            IList<string> tokens = new List<string>();
+            var str = new StringTokenizer(line);
+            var sb = new StringBuilder();
+            var tokens = new List<string>();
             while (str.hasMoreTokens())
             {
-                string tok = str.nextToken();
+                var tok = str.nextToken();
                 tokens.Add(tok);
                 sb.Append(tok).Append(" ");
             }
-            string text = sb.ToString().Substring(0, sb.Length - 1);
-            Parse p = new Parse(text, new Span(0, text.Length), AbstractBottomUpParser.INC_NODE, 0, 0);
-            int start = 0;
-            int i = 0;
-            for (IEnumerator<string> ti = tokens.GetEnumerator(); ti.MoveNext(); i++)
+            var text = sb.ToString().Substring(0, sb.Length - 1);
+            var p = new Parse(text, new Span(0, text.Length), AbstractBottomUpParser.INC_NODE, 0, 0);
+            var start = 0;
+            var i = 0;
+            for (var ti = tokens.GetEnumerator(); ti.MoveNext(); i++)
             {
-                string tok = ti.Current;
+                var tok = ti.Current;
+                if (tok == null) continue;
                 p.insert(new Parse(text, new Span(start, start + tok.Length), AbstractBottomUpParser.TOK_NODE, 0, i));
                 start += tok.Length + 1;
             }
-            Parse[] parses;
-            if (numParses == 1)
-            {
-                parses = new Parse[] { parser.parse(p) };
-            }
-            else
-            {
-                parses = parser.parse(p, numParses);
-            }
-            return parses;
+            return numParses == 1 ? new [] { parser.parse(p) } : parser.parse(p, numParses);
         }
     }
 }
