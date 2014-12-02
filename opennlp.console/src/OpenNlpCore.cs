a32c8b6f47cdcadb1dd39cf43e0b46c967624019
Mark Johnston <mjj@mintydog.com>
2014-12-02 23:31:21 +0000

    Update command line and added input/output streams

--- opennlp.console/src/OpenNlpCore.cs
+++ opennlp.console/src/OpenNlpCore.cs
@@ -1,11 +1,49 @@
-﻿namespace opennlp.console
+﻿using System;
+using System.IO;
+
+namespace opennlp.console
 {
     public class OpenNlpCore
     {
         private readonly Options _options;
+        private StreamReader _inputStream = null;
+        private StreamWriter _outputStream = null;
+
         public OpenNlpCore(Options options)
         {
             _options = options;
+            if (!string.IsNullOrEmpty(_options.InputFilename))
+            {
+                InitializeInputStream();
+            }
+            if (!string.IsNullOrEmpty(_options.OutputFilename))
+            {
+                InitializeOutputStream();
+            }
+        }
+
+        private void InitializeOutputStream()
+        {
+            try
+            {
+                _outputStream = new StreamWriter(_options.OutputFilename);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void InitializeInputStream()
+        {
+            try
+            {
+                _inputStream = new StreamReader(_options.InputFilename);
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
 
         public void Process()
