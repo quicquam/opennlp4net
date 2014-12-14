dee2e11235d6dc15e4e8a16dfed35df4a5bacd1f
Mark Johnston <mjj@mintydog.com>
2014-12-14 18:00:01 +0000

    Start of re-write of console, some training data

---
+++ opennlp.console/src/CmdlineParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace opennlp.console
+{
+    public class CmdlineParser
+    {
+        public Dictionary<string, string> Parse(string[] args)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        private KeyValuePair<string, string> FindToolName(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private KeyValuePair<string, string> FindModelName(string[] args)
+        {
+            throw  new NotImplementedException();
+        }
+
+        private KeyValuePair<string, string> FindOutputFileName(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private KeyValuePair<string, string> FindInputFileName(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<KeyValuePair<string, string>> FindToolParameters(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
