edeaf076990bb50518f42547c1e0762b01dc2dfb
Mark Johnston <mjj@mintydog.com>
2015-01-04 15:04:02 +0000

    First attempt to get EvaluationTests working

---
+++ opennlp.tools.Tests/src/EvaluationTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using opennlp.tools.cmdline.sentdetect;
+
+namespace opennlp.tools.Tests
+{
+    [TestFixture]
+    public class EvaluationTests
+    {
+        private const string ModelPath = @"..\..\data\models\";
+        private const string EvalPath = @"..\..\data\input\eval\";
+
+        [Test]
+        public void SentdetectEvaluationToolReturnsPrecisionRecallAndFMeasure()
+        {
+            var argList = new List<string>
+            {
+                "-model",
+                string.Format("{0}{1}", ModelPath, "en-sent.bin"),
+                "-lang",
+                "en",
+                "-data",
+                string.Format("{0}{1}", EvalPath, "en-sent.eval"),
+                "-encoding",
+                "UTF-8"
+            };
+
+            var evaluator = new SentenceDetectorEvaluatorTool();
+            evaluator.run(null, argList.ToArray());
+        }
+    }
+}
